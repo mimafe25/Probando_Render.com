ba36d1aeb9844170a47c98aa3372c533d28d847a
mafer anonimo <i.biotecn@gmail.com>
2025-06-09 19:29:33 -0600

    Tercer commit

--- python_moneda.py
+++ python_moneda.py
@@ -1,15 +1,3 @@
-import streamlit as st
-
-st.header('Lanzar una moneda')
-
-number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
-start_button = st.button('Ejecutar')
-
-if start_button:
-    st.write(f'Experimento con {number_of_trials} intentos en curso.')
-
-st.write('Esta aplicación aún no es funcional. En construcción.')
-
 import pandas as pd
 import scipy.stats
 import streamlit as st
